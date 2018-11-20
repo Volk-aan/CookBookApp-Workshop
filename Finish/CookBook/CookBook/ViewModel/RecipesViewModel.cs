5ba362aed072750db321eda72aa4f3ef315c2acd
Frédéric Carbonnelle <f.carbonnelle@gmail.com>
2018-11-20 14:49:53 +0100

    Custom vision simple implementation

--- Finish/CookBook/CookBook/ViewModel/RecipesViewModel.cs
+++ Finish/CookBook/CookBook/ViewModel/RecipesViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.Models;
+
+using System;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Essentials;
@@ -7,6 +11,11 @@ using System.Linq;
 using CookBook.Model;
 using System.Diagnostics;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.IO;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.Models;
 
 namespace CookBook.ViewModel
 {
@@ -15,15 +24,17 @@ namespace CookBook.ViewModel
         public ObservableCollection<Recipe> Recipes { get; }
         public Command GetRecipesCommand { get; }
         public Command GetClosestCommand { get; }
+        public Command GetByPictureCommand { get; }
         public RecipesViewModel()
         {
             Title = "Recipes";
             Recipes = new ObservableCollection<Recipe>();
             GetRecipesCommand = new Command(async () => await GetRecipesAsync());
             GetClosestCommand = new Command(async () => await GetClosestAsync());
+            GetByPictureCommand = new Command(async () => await GetRecipeByImage());
         }
 
-    
+
         async Task GetRecipesAsync()
         {
             if (IsBusy)
@@ -67,7 +78,7 @@ namespace CookBook.ViewModel
                 }
 
                 var first = Recipes.OrderBy(m => location.CalculateDistance(
-                    new Location(m.Latitude, m.Longitude), DistanceUnits.Miles))
+                    new Xamarin.Essentials.Location(m.Latitude, m.Longitude), DistanceUnits.Miles))
                     .FirstOrDefault();
 
                 await Application.Current.MainPage.DisplayAlert("", first.Name + " " +
@@ -81,6 +92,75 @@ namespace CookBook.ViewModel
             }
         }
 
+        private async Task GetRecipeByImage()
+        {
+            try
+            {
+                var image = await TakePhoto();
+                if(image != null)
+                {
+                    var tag = await GetTagByPictureAsync(image);
+                    await Application.Current.MainPage.DisplayAlert("Ca a l'air bon !", $"On affiche des recette de {tag.TagName} ? ({tag.Probability:P1} sûr)", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to get tags: {ex.Message}");
+                await Application.Current.MainPage.DisplayAlert("Error!", ex.Message, "OK");
+            }
+        }
 
+        private async Task<Stream> TakePhoto()
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error!", "Aucun appareil photo détecté", "Votre appareil ne dispose pas ou ne trouve pas d'appareil photo.");
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                CompressionQuality = 92,
+            });
+
+            Stream fileStream = file.GetStream();
+
+            return fileStream;
+        }
+
+        async Task<PredictionModel> GetTagByPictureAsync(Stream image)
+        {
+            string SouthCentralUsEndpoint = "https://southcentralus.api.cognitive.microsoft.com";
+
+            // Add your training & prediction key from the settings page of the portal
+            string trainingKey = "2879f3d6240a4805bffe13b64758e02c";
+            string predictionKey = "ddbeaf61ee2e4ef99432de15336630d1";
+
+            // Create the Api, passing in the training key
+            CustomVisionTrainingClient trainingApi = new CustomVisionTrainingClient()
+            {
+                ApiKey = trainingKey,
+                Endpoint = SouthCentralUsEndpoint
+            };
+
+            // Create a new project
+            Guid projectId = new Guid("1bb9aeab-ce73-4025-947a-e04f216ea804");
+            var project = trainingApi.GetProject(projectId);
+
+            // Now there is a trained endpoint, it can be used to make a prediction
+
+            // Create a prediction endpoint, passing in obtained prediction key
+            CustomVisionPredictionClient endpoint = new CustomVisionPredictionClient()
+            {
+                ApiKey = predictionKey,
+                Endpoint = SouthCentralUsEndpoint
+            };
+
+            // Make a prediction against the new project
+            Console.WriteLine("Making a prediction:");
+            var result = endpoint.PredictImage(project.Id, image);
+            var tag = result.Predictions.FirstOrDefault();
+
+            return tag;
+        }
     }
 }
